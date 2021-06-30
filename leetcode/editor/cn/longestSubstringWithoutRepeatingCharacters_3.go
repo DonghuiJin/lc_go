cf34a64c969b3105e42750faffef0601764c48b8
victoryjin <victoryjin@tencent.com>
2021-06-30 17:30:26 +0800

    feat: 3.最长子序列

---
+++ leetcode/editor/cn/longestSubstringWithoutRepeatingCharacters_3.go
@@ -0,0 +1,33 @@
+package cn
+
+//leetcode submit region begin(Prohibit modification and deletion)
+// 滑动窗口
+func lengthOfLongestSubstring(s string) int {
+	// 构建一个map
+	m := map[byte]int{}
+	n := len(s)
+	rk, ans := -1, 0
+	for i := 0; i < n; i++ {
+		if i != 0 {
+			// 左指针向右移动一格，移除一个字符
+			delete(m, s[i-1])
+		}
+		for rk+1 < n && m[s[rk+1]] == 0 {
+			// 不断的右移动指针
+			m[s[rk+1]]++
+			rk++
+		}
+		// 第 i 到 rk 个字符是一个极长的无重复字符子串
+		ans = max(ans, rk-i+1)
+	}
+	return ans
+}
+
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
