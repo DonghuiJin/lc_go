ab09d645a42a870b5c19fb73f95b986319f9b01f
victoryjin <victoryjin@tencent.com>
2021-06-30 17:30:02 +0800

    feat: 3.最长子序列

---
+++ leetcode/editor/cn/longestSubstringWithoutRepeatingCharacters_3_test.go
@@ -0,0 +1,27 @@
+package cn
+
+import "testing"
+
+func Test_lengthOfLongestSubstring(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{name: "case1", args: args{s: "abcabcbb"}, want: 3},
+		{name: "case2", args: args{s: "bbbbb"}, want: 1},
+		{name: "case3", args: args{s: "pwwkew"}, want: 3},
+		{name: "case3", args: args{s: ""}, want: 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLongestSubstring(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLongestSubstring() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
